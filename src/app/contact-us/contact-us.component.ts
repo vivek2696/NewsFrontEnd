b9f379ce40da0bbe711255766ab12d003787ea7e
Francis K <frageokiz5@gmail.com>
2021-05-20 22:36:49 -0400

    presentation stuff

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,13 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import {  HostListener } from '@angular/core';
+import { HostListener } from '@angular/core';
 import { ConnectionService } from '../services/connection.service';
-
 
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
-  styleUrls: ['./contact-us.component.css']
+  styleUrls: ['./contact-us.component.css'],
 })
 export class ContactUsComponent implements OnInit {
   contactForm: FormGroup;
@@ -15,32 +14,33 @@ export class ContactUsComponent implements OnInit {
 
   //Listener for checking if form is valid or not
   @HostListener('input') oninput() {
-
     if (this.contactForm.valid) {
       this.disabledSubmitButton = false;
-      }
+    }
   }
 
-
-
-  constructor(private fb: FormBuilder, private connectionService: ConnectionService) {
+  constructor(
+    private fb: FormBuilder,
+    private connectionService: ConnectionService
+  ) {
     //Contact form builder
     this.contactForm = fb.group({
-      'contactFormEmail': ['', [Validators.required, Validators.email]],
-      'contactFormMessage': ['', Validators.required],
-      });
-   }
-
-  onSubmit() {
-    this.connectionService.sendMessage(this.contactForm.value).subscribe(() => {
-      alert('Your message has been sent.');
-      this.contactForm.reset();
-    }, error => {
-      console.log('Error', error);
+      email: ['', [Validators.required, Validators.email]],
+      query: ['', Validators.required],
     });
   }
 
-  ngOnInit(): void {
+  onSubmit() {
+    this.connectionService.sendMessage(this.contactForm.value).subscribe(
+      () => {
+        alert('Your message has been sent.');
+        this.contactForm.reset();
+      },
+      (error) => {
+        console.log('Error', error);
+      }
+    );
   }
 
+  ngOnInit(): void {}
 }
