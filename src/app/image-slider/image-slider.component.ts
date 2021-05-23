a7d5cddee0ddd01f9b712e8886accd3f198d1ef7
Francis K <frageokiz5@gmail.com>
2021-05-22 21:00:55 -0400

    Chat location, image-slider title, and news article changes

--- src/app/image-slider/image-slider.component.ts
+++ src/app/image-slider/image-slider.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { NewsInterface } from '../models/news.model';
+import { NewsService } from '../services/news.service';
 
 @Component({
   selector: 'image-slider',
   templateUrl: './image-slider.component.html',
-  styleUrls: ['./image-slider.component.css']
+  styleUrls: ['./image-slider.component.css'],
 })
 export class ImageSliderComponent implements OnInit {
+  newsList: NewsInterface[];
 
-  constructor() { }
+  constructor(private newsService: NewsService) {}
 
   ngOnInit(): void {
+    this.newsService.getAllRegularNews().subscribe((newsArray) => {
+      // TODO have to sort based on time for latest news
+      //let tempArray = newsArray.sort(a)
+      this.newsList = newsArray;
+    });
   }
-
 }
