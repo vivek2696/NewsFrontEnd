72382ed320e318fe687226a2dfea2bdd5730ade7
ranjitjohn01 <38595565+ranjitjohn01@users.noreply.github.com>
2021-05-24 13:37:10 -0700

    sportsPage Works!!!!

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -7,6 +7,7 @@ import { Injectable } from '@angular/core';
 export class NewsService {
   url = 'http://localhost:3400/api/';
   private baseUrl = 'http://localhost:3400/api/regular-news';
+  private baseUrl2 = 'http://localhost:3400/api/sports-news';
 
   constructor(private http: HttpClient) {}
 
@@ -22,5 +23,9 @@ export class NewsService {
 
   getAllRegularNews() {
     return this.http.get<any[]>(this.baseUrl);
+  }
+
+  getAllSportsNews() {
+    return this.http.get<any[]>(this.baseUrl2);
   }
 }
